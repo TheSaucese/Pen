2e7d2f39bacb84b65f3211245acde6ee9bf9436b
asmae255 <asmaeamakrane615@gmail.com>
2023-07-26 07:06:48 -0400

    hh

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-tytfyufu
+#1/ import of libraries---------------
+#2/ function needed : a/Web Scraping Functions:url,forms...
+#_____________________b/Network Scanning Functions:ports ,proto
+#_____________________c/Subdomain Enumeration Functions:
+#_____________________d/Function for WordPress Detection:
+#_____________________e/function for WordPress Version Detection 
+#_____________________f/check subdomain status (200ok or 401 forbiden)
+import requests
+from bs4 import BeautifulSoup
+import nmap
+import sublist3r
+import python_nmap
