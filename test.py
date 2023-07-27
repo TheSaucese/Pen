f941c6d45ff24e0ac44c7e5fe0e614bfdf5d56c0
asmae255 <asmaeamakrane615@gmail.com>
2023-07-27 07:35:26 -0400

    hh

--- test.py
+++ test.py
@@ -11,15 +11,24 @@ import requests
 from bs4 import BeautifulSoup
 import nmap
 from if_wdpress import check_wordpress
-from version import get_wordpress_version
+#from version import get_wordpress_version
 from scrabe import scrape_website
-from ip import get_website_info
+#from ip import get_website_info
+from get_ports import get_open_ports
 
 if __name__ == "__main__":
     target_url = input("Enter the target URL: ")
     scrape_website(target_url)
-    ip_address, server = get_website_info(target_url)
+   # ip_address, server = get_website_info(target_url)
+    open_ports = get_open_ports(target_url)
     if check_wordpress(target_url):
         print("The website is built with WordPress.")
     else:
         print("The website is not built with WordPress.")
+
+    if open_ports:
+        print(f"Open Ports and Services for {target_url}:")
+        for port, service in open_ports.items():
+           print(f"Port: {port}, Service: {service}")
+    else:
+        print("No open ports found or unable to retrieve information.")
