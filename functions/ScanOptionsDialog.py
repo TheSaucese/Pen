72687d5046aa1d78eb9fa24b225be9d05cc89032
TheSaucese <selfarricha2003@gmail.com>
2023-08-08 10:55:44 +0100

    a

--- functions/ScanOptionsDialog.py
+++ functions/ScanOptionsDialog.py
@@ -1,11 +1,11 @@
-from PySide6.QtWidgets import QDialog, QVBoxLayout, QCheckBox, QDialogButtonBox
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QCheckBox, QDialogButtonBox,QLabel
 from PySide6.QtCore import Qt
 
 class ScanOptionsDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Scan Options")
-        self.setFixedSize(200, 150)
+        self.setFixedSize(250, 150)
        
 
         layout = QVBoxLayout(self)
@@ -23,11 +23,24 @@ class ScanOptionsDialog(QDialog):
         layout.addWidget(self.wordpress_scan_checkbox)
         layout.addWidget(self.url_scan_checkbox)
 
+        self.message_label = QLabel("You must select at least one option.")
+        self.message_label.setAlignment(Qt.AlignCenter)
+        self.message_label.setStyleSheet("color: red;")
+        self.message_label.hide()
+        layout.addWidget(self.message_label)    
+
         # Add dialog buttons
         button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        button_box.accepted.connect(self.accept)
+        button_box.accepted.connect(self.validate_and_accept)
         button_box.rejected.connect(self.reject)
         layout.addWidget(button_box)
+
+    def validate_and_accept(self):
+        selected_options = self.get_selected_options()
+        if selected_options:
+            self.accept()
+        else:
+            self.message_label.show()
 
     def get_selected_options(self):
         selected_options = []
